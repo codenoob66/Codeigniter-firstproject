52e80e6c3d417b4b84cbe6d4f664e1d500c9f493
rafael vincent <d749107@gmail.com>
2025-12-29 12:44:06 +0800

    fixes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,9 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-<<<<<<< HEAD
 $routes->get('/contact', 'contact::index');
-=======
-$routes->get('/dashboard', 'DashboardController::index');
-$routes->get('/hello', 'HelloController::index');
->>>>>>> 642315af09bcf15252308e943032df6621d64026
+
+
+
