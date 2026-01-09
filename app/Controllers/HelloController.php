785d533f67321a6a069b80a5469d981c74269112
rafael vincent <d749107@gmail.com>
2026-01-09 10:19:36 +0800

    just starting

--- app/Controllers/HelloController.php
+++ app/Controllers/HelloController.php
@@ -6,12 +6,6 @@ class HelloController extends BaseController
 {
     public function index()
     {
-        $data['title'] = "Welcome to CodeIgniter";
-        $data['message'] = "This is my first page!";
-        $data['meow'] = "test";
-        $itot['meow'] = "test";
-
-        // This sends the $data array to the view file
-        return view('hello_view', $data, $itot);
+        return view("dashboard");
     }
 }
