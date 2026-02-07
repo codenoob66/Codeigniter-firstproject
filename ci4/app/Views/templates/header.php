3dd9ca1a8b858504fc0d4fabbf8086ed69bc1798
rafael vincent <d749107@gmail.com>
2026-02-07 19:34:54 +0800

    uncommented head meta tag

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -7,7 +7,7 @@ $uri = $_SERVER['REQUEST_URI'];
 <head>
     <meta charset="utf-8">
     <title><?php echo $title; ?></title>
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
