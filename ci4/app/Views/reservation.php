29c666217833675f55cfe7a5f76c3c33731139c5
rafael vincent <d749107@gmail.com>
2026-02-08 16:04:25 +0800

    removed the test text change in reservation.php

--- ci4/app/Views/reservation.php
+++ ci4/app/Views/reservation.php
@@ -1,5 +1,5 @@
 <div class="d-flex flex-column text-center">
-    <h1 class="text-danger">Walk-ins are welcome!</h1>
+    <h1>Walk-ins are welcome!</h1>
     <p>
         Online reservations are not available at the moment.
         Please visit us directly at the restaurant.
