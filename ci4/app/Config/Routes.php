e6c7e669cae5b5ba2715be2252a4250ef4566b6a
rafael vincent <d749107@gmail.com>
2026-03-11 05:40:33 +0800

    working on email functionality

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -6,6 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->post('/send', 'Home::send');
 $routes->get('/contact', 'Contact::index');
 $routes->get('/reservation', 'Reservation::index');
 $routes->get('/menu', 'Menu::index');
